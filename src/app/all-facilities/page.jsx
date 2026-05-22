abe37482191ab1d9b1e359b2e96d4fe51c5cc5da
Masud76301 <khaledmasud76301@gmail.com>
2026-05-22 15:09:37 +0600

    Search functionality add

--- src/app/all-facilities/page.jsx
+++ src/app/all-facilities/page.jsx
@@ -1,18 +1,24 @@
 import FacilityCard from '@/component/FacilityCard';
+import SearchBar from '@/component/SearchBar';
 import React from 'react';
 
-const allFacilitiesPage = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/facility`);
+const allFacilitiesPage = async ({searchParams}) => {
+        const {search} = await searchParams
+        const facilitySearch = search || "" ;
+    // const search = await searchParams.search || "";
+    
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/facility?search=${facilitySearch}`,{ cache: "no-store" });
     const facilities = await res.json();
     return (
         <div className='container mx-auto my-10'>
             <h1 className='text-4xl text-center font-bold mt-10 text-shadow-2xs mb-10'>All <span className='font-bold text-blue-800'>FACILITIES</span> </h1>
-            
-                <div className='grid grid-cols-4 gap-6 auto-rows-fr'>
-                    {
-                        facilities.map(facility => <FacilityCard key={facility._id} facility={facility}></FacilityCard>)
-                    }
-                </div>
+            <SearchBar></SearchBar>
+
+            <div className='grid grid-cols-4 gap-6 auto-rows-fr'>
+                {
+                    facilities.map(facility => <FacilityCard key={facility._id} facility={facility}></FacilityCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/component/SearchBar.jsx
@@ -0,0 +1,48 @@
+
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+
+
+
+const SearchBar = () => {
+  const router =useRouter();
+  const params = useSearchParams();
+
+  const defaultValue = params.get("search")||"";
+  const [text, setText] =useState(defaultValue);
+
+  const handleSearch = () => {
+    const query =text.trim();
+    router.push(`/all-facilities?search=${query}`);
+  }
+
+  return (
+    <div className="relative flex items-center  bg-white border border-slate-200 rounded-xl shadow-sm focus-within:ring-4 focus-within:ring-blue-600/10 focus-within:border-blue-600 transition-all overflow-hidden my-6 w-[48%] h-12">
+
+      <div className="pl-5 text-slate-400">
+       <CiSearch />
+      </div>
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+        placeholder="Search for facility name"
+        className="flex-1 h-14 px-4 outline-none bg-transparent text-slate-700 placeholder:text-slate-400"
+      />
+
+      <button
+        onClick={handleSearch}
+        className="h-8 px-6 mr-2 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-700 transition-colors"
+
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
